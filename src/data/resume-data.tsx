6a0791002f18305c1602a97a18237a05a843df54
Celiz <maticelizramos@gmail.com>
2024-12-18 22:47:48 -0300

    se

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,121 +1,126 @@
 import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
-} from "@/images/logos";
-import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
-
-export const RESUME_DATA = {
-  name: "Matías Nicolás Celiz Ramos",
-  initials: "MC",
-  location: "Mar del Plata, Buenos Aires, Argentina, UTC-3",
-  locationLink: "https://www.google.com/maps/place/Mar+del+Plata",
-  about:
-    "Full Stack Junior",
-  summary:
-    "Soy una persona proactiva, organizada y responsable, con buenas relaciones interpersonales. Siempre tengo la mejor disposición para la realización de mis labores. Busco un puesto desafiante donde pueda continuar aprendiendo.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/82237565?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
-  contact: {
-    email: "maticelizramos@gmail.com",
-    tel: "+542235421371",
-    social: [
+    AmbitLogo,
+    BarepapersLogo,
+    BimLogo,
+    CDGOLogo,
+    ClevertechLogo,
+    ConsultlyLogo,
+    EvercastLogo,
+    Howdy,
+    JarockiMeLogo,
+    JojoMobileLogo,
+    Minimal,
+    MobileVikingsLogo,
+    MonitoLogo,
+    NSNLogo,
+    ParabolLogo,
+    TastyCloudLogo,
+    YearProgressLogo,
+  } from "@/images/logos";
+  import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+  
+  export const RESUME_DATA = {
+    name: "Matías Nicolás Celiz Ramos",
+    initials: "MC",
+    location: "Mar del Plata, Buenos Aires, Argentina, UTC-3",
+    locationLink: "https://www.google.com/maps/place/Mar+del+Plata",
+    about:
+      "Full Stack Junior",
+    summary:
+      "Soy una persona proactiva, organizada y responsable, con buenas relaciones interpersonales. Siempre tengo la mejor disposición para la realización de mis labores. Busco un puesto desafiante donde pueda continuar aprendiendo.",
+    avatarUrl: "https://avatars.githubusercontent.com/u/82237565?v=4",
+    personalWebsiteUrl: "https://celizin.dev.ar",
+    contact: {
+      email: "maticelizramos@gmail.com",
+      tel: "+542235421371",
+      social: [
+        {
+          name: "GitHub",
+          url: "https://github.com/Celiz",
+          icon: GitHubIcon,
+        },
+        {
+          name: "LinkedIn",
+          url: "https://www.linkedin.com/in/celizm/",
+          icon: LinkedInIcon,
+        },
+      ],
+    },
+    education: [
       {
-        name: "GitHub",
-        url: "https://github.com/Celiz",
-        icon: GitHubIcon,
-      },
-      {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/celizm/",
-        icon: LinkedInIcon,
+        school: "Escuela de Educacion Secundaria Tecnica Nº5 Amancio Williams",
+        degree: "Técnico en Informática Personal y Profesional",
+        start: "2018",
+        end: "2024",
       },
     ],
-  },
-  education: [
-    {
-      school: "Escuela de Educacion Secundaria Tecnica Nº5 Amancio Williams",
-      degree: "Técnico en Informática Personal y Profesional",
-      start: "2018",
-      end: "2024",
-    },
-  ],
-  work: [
-    {
-      company: "TapWise (proyecto en curso)",
-      link: "",
-      badges: ["Remoto"],
-      title: "Junior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "Marzo 2024",
-      end: "Actualidad",
-      description:
-        "Creación de tienda con Hydrogen , aplicación para mobile en ionic, implementación de nuevas características. Tecnologías:  Hydrogen, TypeScript, Shopify, Ionic Capacitor, SCSS",
-    },
-    {
-      company: "FinGuru",
-      link: "http://cursos-staging.fin.guru/es",
-      badges: ["Remoto"],
-      title: "Junior Full Stack Developer",
-      logo: MonitoLogo,
-      start: "Marzo 2024",
-      end: "Mayo 2024",
-      description:
-        "Implementación de nuevas características, trabajo en equipo. Tecnologías: React, TypeScript, Strapi, SCSS",
-    },
-    {
-      company: "dydx bot",
-      link: "",
-      badges: ["Remoto"],
-      title: "Junior Backend Developer",
-      logo: ConsultlyLogo,
-      start: "Febrero 2024",
-      end: "Marzo 2024",
-      description:"Bot de uso interno que se encargaba de arbitrar tasas de interés de diferentes protocolos DeFi en redes EVM y Cosmos. Tecnologías: Fastify, Ethers.js v5 y v6, Smart contracts (EVM y Cosmos)",
-    },
-    {
-      company: "Tranqui",
-      link: "https://tranqui.mdp.edu.ar/",
-      badges: ["Presencial"],
-      title: "Junior Frontend Developer",
-      logo: ConsultlyLogo,
-      start: "Octubre 2023",
-      end: "Actualidad",
-      description:"Creación de app mobile,desarrollo de nuevas características, mantenimiento de la aplicación, trabajo en equipo. Tecnologías: React, TypeScript, Ionic, SCSS",
-    },
-    
-  ],
-  skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js",
-    "Node.js",
-    "CSS/SCSS",
-    "GitHub",
-    "C++",
-    "Python",
-    "Java",
-    "Ionic",
-    "Capacitor",
-    "Fastify",
-    "Ethers.js",
-    "Smart contracts",
-  ],
+    work: [
+      {
+        company: "Grucar Remolques",
+        link: "",
+        badges: ["Remoto"],
+        title: "Junior Full Stack Developer",
+        logo: CDGOLogo,
+        start: "Junio 2024",
+        end: "Octubre 2024",
+        description:
+          "Participación integral en el desarrollo de un sistema interno para una empresa de grúas, abarcando tanto el diseño de la interfaz como su desarrollo. Tecnologías: Next.js, TypeScript, Tailwind CSS, Supabase (PostgreSQL)",
+      },
+      {
+        company: "FinGuru",
+        link: "http://cursos-staging.fin.guru/es",
+        badges: ["Remoto"],
+        title: "Junior Full Stack Developer",
+        logo: MonitoLogo,
+        start: "Marzo 2024",
+        end: "Mayo 2024",
+        description:
+          "Implementación de nuevas características, integración de la API de Mercado Pago, desarrollo de endpoints para la gestión de reseñas de cursos. Tecnologías: React, TypeScript, Strapi, SCSS",
+      },
+      {
+        company: "Tranqui",
+        link: "https://tranqui.mdp.edu.ar/",
+        badges: ["Presencial"],
+        title: "Junior Frontend Developer",
+        logo: ConsultlyLogo,
+        start: "Octubre 2022",
+        end: "Mayo 2024",
+        description:"Creación de app mobile, desarrollo de nuevas características, mantenimiento de la aplicación, trabajo en equipo. Tecnologías: React, TypeScript, Ionic, SCSS",
+      },
+    ],
+    skills: [
+      "JavaScript",
+      "TypeScript",
+      "React/Next.js",
+      "Node.js",
+      "CSS/SCSS",
+      "GitHub",
+      "C++",
+      "Python",
+      "Java",
+      "Ionic",
+      "Capacitor",
+      "Fastify",
+      "Ethers.js",
+      "Smart contracts",
+      "Supabase",
+      "MySQL",
+      "PostgreSQL",
+      "Mercado Pago API"
+    ],
+    languages: [
+      {
+        name: "Español",
+        level: "Nativo",
+      },
+      {
+        name: "Inglés",
+        level: "A2",
+      },
+      {
+        name: "Portugués",
+        level: "A1",
+      },
+    ],
+  } as const;
   
-   
-
-} as const;
